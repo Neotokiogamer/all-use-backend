4062d0ca3d7fe62d609b9d4799ecd909dec57a11
Jose Escobar <94930719+Neotokiogamer@users.noreply.github.com>
2022-09-13 03:49:52 -0300

    Fin seccion 10 - Auth

---
+++ helpers/jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+
+const generarJWT = ( uid ) =>{
+
+    return new Promise( (resolve, reject) =>{
+
+        const payload = {
+            uid,
+        }
+    
+        jwt.sign( payload, process.env.JWT_SECRET, {
+            expiresIn: '30d'
+        }, ( err, token ) =>{
+    
+            if( err ) {
+                console.log(err);
+                reject( 'No se pudo generar el JWT' );
+            }else {
+                resolve( token );
+            }
+        });
+    
+    });
+
+}
+
+module.exports = {
+    generarJWT,
+}
