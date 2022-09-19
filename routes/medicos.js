30591450f5305bdbb539cb61ddd1c2d88753a8c6
Jose Escobar <94930719+Neotokiogamer@users.noreply.github.com>
2022-09-19 09:52:43 -0300

    Fin seccion 11

---
+++ routes/medicos.js
@@ -0,0 +1,52 @@
+/*
+    medicos
+    ruta: '/api/medico'
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos')
+
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const {
+    getMedicos,
+    crearMedico,
+    actualizarMedico,
+    borrarMedico,
+} = require('../controllers/medicos')
+
+
+const router = Router();
+
+
+router.get('/',  getMedicos);
+
+router.post('/',
+    [
+        
+        validarJWT,
+        check('nombre','El nombre del medico es necesario').not().isEmpty(),
+        check('hospital','El hospital id debe ser valido').isMongoId(),
+        validarCampos
+    ],
+    crearMedico
+);
+
+
+router.put('/:id',
+    [
+      
+    ],
+    actualizarMedico
+);
+
+// borrar Usuario
+router.delete('/:id',
+    borrarMedico
+);
+
+
+
+module.exports = router;
+
